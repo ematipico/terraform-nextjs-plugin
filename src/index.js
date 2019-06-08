d75cde366e5b54dd92492b9d965bd21e1b414843
Emanuele Stoppa <my.burning@gmail.com>
2019-06-08 12:25:48 +0100

    feat: read configuration from config file

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ const {
 	generateTerraformConfiguration
 } = require("./generateApiGatewayConfiguration");
 const { generateLambdas } = require("./generateLambdas");
+const cosmiconfig = require("cosmiconfig");
+
 /**
  *
  * @param {*} configuration The configuration needed to generate the resources
  * @param {boolean} [write=false]
  */
-function generateResources(configuration, write = false) {
+async function generateResources(configuration, write = false) {
 	try {
-		checkConfiguration(configuration);
+		const fileConfiguration = retrieveConfiguration();
+		checkConfiguration({
+			...fileConfiguration,
+			...configuration
+		});
 		setConfiguration(configuration);
 		if (write === true) {
 			generateTerraformConfiguration(write);
@@ -25,6 +31,16 @@ function generateResources(configuration, write = false) {
 		// eslint-disable-next-line no-console
 		console.error(error.message);
 		process.exit(1);
+	}
+}
+
+async function retrieveConfiguration() {
+	const explorer = cosmiconfig("terranext");
+	try {
+		const result = await explorer.search();
+		return result.config;
+	} catch (error) {
+		return undefined;
 	}
 }
 
